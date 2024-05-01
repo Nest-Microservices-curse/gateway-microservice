360f614ba88c3a776876a5afba865e10464b3078
Daniel <dmiramontesg@gmail.com>
2024-05-01 16:48:51 -0600

    modify order dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,23 +1,30 @@
 
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrderStatus, OrderStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { OrderItemDto } from "./index";
+import { Type } from "class-transformer";
+
 export class CreateOrderDto {
 
-    @IsNumber()
-    @IsPositive()
-    totalAmount: number;
+    // @IsNumber()
+    // @IsPositive()
+    // totalAmount: number;
 
-    @IsNumber()
-    @IsPositive()
-    totalItems: number;
+    // @IsNumber()
+    // @IsPositive()
+    // totalItems: number;
 
-    @IsEnum(OrderStatusList, {
-        message: `Possible status values are ${ OrderStatusList}`
-    })
-    @IsOptional()
-    status: OrderStatus = OrderStatus.PENDING;
+    // @IsEnum(OrderStatusList, {
+    //     message: `Possible status values are ${ OrderStatusList}`
+    // })
+    // @IsOptional()
+    // status: OrderStatus = OrderStatus.PENDING;
 
-    @IsBoolean()
-    paid: boolean = false;
+    // @IsBoolean()
+    // paid: boolean = false;
+    @IsArray()
+    @ArrayMinSize(1)
+    @ValidateNested({ each: true})
+    @Type( () =>  OrderItemDto)
+    items: OrderItemDto[];
 
 }
